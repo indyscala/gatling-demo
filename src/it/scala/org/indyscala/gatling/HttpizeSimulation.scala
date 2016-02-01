d4014564c9ba039026fbaa729c4481f9159a7480
Brad Fritz <brad@fewerhassles.com>
2016-02-01 01:16:07 -0500

    better simulation for httpize
    Currently testing:
    
     * /ip
     * /delay
     * /stream
     * /form
     * /post

--- src/it/scala/org/indyscala/gatling/HttpizeSimulation.scala
+++ src/it/scala/org/indyscala/gatling/HttpizeSimulation.scala
@@ -1,71 +1,43 @@
 package org.indyscala.gatling
 
+import io.gatling.core.structure.PopulatedScenarioBuilder
+import io.gatling.core.structure.ScenarioBuilder
+
 import scala.concurrent.duration._
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
-import io.gatling.jdbc.Predef._
+
+import java.util.concurrent.ThreadLocalRandom
 
 class HttpizeSimulation extends Simulation {
+
+  val Ipv4Localhost = "127.0.0.1"
+  val Ipv6Localhost = "0:0:0:0:0:0:0:1"
+  val Localhost = Ipv4Localhost
 
   val httpProtocol = http
     .baseURL("http://127.0.0.2:8080")
     .inferHtmlResources()
 
-  val headers_1 = Map("Accept" -> "image/png,image/*;q=0.8,*/*;q=0.5")
+  val ip = scenario("Httpize - IP")
+    .exec(http("ip")
+      .get("/ip")
+      .check(jsonPath("$.remote").is(Localhost)))
 
-    val uri1 = "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
-    val uri2 = "http://127.0.0.2:8080"
+  val random_delay = scenario("Httpize - Delay"  )
+    .exec(http("delay")
+      .get(session => "/delay/" + ThreadLocalRandom.current.nextInt(10)))
 
-  val scn = scenario("HttpizeSimulation")
-    .exec(http("request_0")
-      .get("/")
-      .resources(http("request_1")
-      .get(uri2 + "/favicon.ico")
-      .headers(headers_1)
-      .check(status.is(404)),
-            http("request_2")
-      .get(uri2 + "/favicon.ico")
-      .check(status.is(404))))
-    .pause(18)
-    // Ip
-    .exec(http("request_3")
-      .get("/ip"))
-    .pause(15)
-    // Headers
-    .exec(http("request_4")
-      .get("/headers"))
-    .pause(46)
-    // Cookie Set
-    .exec(http("request_5")
-      .get("/cookies/set?foo=bar"))
-    .pause(11)
-    // Cookie Get
-    .exec(http("request_6")
-      .get("/cookies"))
-    .pause(18)
-    // Home
-    .exec(http("request_7")
-      .get("/"))
-    .pause(27)
-    // Stream
-    .exec(http("request_8")
-      .get("/stream/20"))
-    .pause(16)
-    // Delay
-    .exec(http("request_9")
-      .get("/delay/3"))
-    .pause(15)
-    // HTML Page
-    .exec(http("request_10")
-      .get("/moby"))
-    .pause(20)
-    // Form Get
-    .exec(http("request_11")
+  val random_stream = scenario("Httpize - Stream"  )
+      .exec(http("stream")
+          .get(session => "/stream/" + ThreadLocalRandom.current.nextInt(100)))
+
+  val form_post = scenario("Httpize - Form")
+    .exec(http("GET")
       .get("/form"))
-    .pause(46)
-    // Form POST
-    .exec(http("request_12")
+    .pause(3)
+    .exec(http("POST")
       .post("/post")
       .formParam("custname", "a")
       .formParam("custtel", "b")
@@ -76,5 +48,13 @@ class HttpizeSimulation extends Simulation {
       .formParam("delivery", "1:00")
       .formParam("comments", "Deliver it!"))
 
-  setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
+  setUp(List(ip, random_delay, random_stream, form_post).map(populateScenario))
+
+  def populateScenario(sb: ScenarioBuilder): PopulatedScenarioBuilder = {
+    sb.inject(
+      nothingFor(1 seconds),
+      atOnceUsers(10),
+      rampUsers(10) over(5 seconds)
+    ).protocols(httpProtocol)
+  }
 }
